7e2143359c108b2cbc35ac2224cf44c67c0854ac
Simran Parween <simranparween2754@gmail.com>
2025-07-20 19:34:10 +0530

    new solution added

---
+++ Array/Remove_element.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3, 2, 2, 3};  // Sample input
+    int val = 3;
+
+    int newLength = sol.removeElement(nums, val);
+
+    cout << "New length: " << newLength << endl;
+    cout << "Modified array: ";
+    for (int i = 0; i < newLength; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
