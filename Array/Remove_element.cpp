7c62bbdd78e8ddb4b7bba13c6029f42e5b1c0b8a
Simran Parween <simranparween2754@gmail.com>
2025-07-22 09:38:43 +0530

    modified changes

--- Array/Remove_element.cpp
+++ Array/Remove_element.cpp
@@ -2,30 +2,39 @@
 #include <vector>
 using namespace std;
 
+
 class Solution {
 public:
+  
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
+        int k = 0;  // This will keep track of the next position to place a valid element
 
         for (int i = 0; i < nums.size(); i++) {
+            // If the current element is not equal to the value we want to remove
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+                nums[k] = nums[i];  // Move it to the front of the array
+                k++;  // Move the write pointer ahead
             }
+            // If nums[i] == val, we simply skip it 
         }
 
+        // After the loop, 'k' is the new length of the array without 'val'
         return k;
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> nums = {3, 2, 2, 3};  // Sample input
+    vector<int> nums = {3, 2, 2, 3};
     int val = 3;
 
+    // Call the removeElement function
     int newLength = sol.removeElement(nums, val);
 
+    // Output the new length of the array
     cout << "New length: " << newLength << endl;
+
+    // Print the modified array 
     cout << "Modified array: ";
     for (int i = 0; i < newLength; i++) {
         cout << nums[i] << " ";
--- Array/Two_sum.cpp
+++ Array/Two_sum.cpp
@@ -4,27 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
+
+vector<int> twoSum(vector<int>& nums, int target) {
+
+    for (int i = 0; i < nums.size(); i++) {
+            // Check the rest of the elements after the current one
             for (int j = i + 1; j < nums.size(); j++) {
+                // If the sum of the two numbers equals the target
                 if (nums[i] + nums[j] == target) {
+                    // Return their indices as a vector
                     return {i, j};
                 }
             }
         }
+        // If no such pair is found, return an empty vector
         return {};
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> nums = {2, 7, 11, 15}; // Sample input
+    vector<int> nums = {2, 7, 11, 15};
     int target = 9;
 
+    // Call the twoSum function and store the result
     vector<int> result = sol.twoSum(nums, target);
 
+    // Check if a valid result was found
     if (!result.empty()) {
         cout << "Indices: " << result[0] << ", " << result[1] << endl;
+        // Output should be: Indices: 0, 1 because 2 + 7 = 9
     } else {
         cout << "No two numbers add up to the target." << endl;
     }
