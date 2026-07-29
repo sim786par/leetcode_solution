0bdc472882b049567df24a6958a8d58d5ecb0871
Simran Parween <simranparween2754@gmail.com>
2026-07-29 16:00:28 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.43%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        int n = image[0].size();
-        for(int i=0;i<image.size();i++){
-            int left =0, right = n-1;
-            while(left <= right){
-                if(left == right){
-                    image[i][left] ^= 1;
-                }
-                else{
-                    //swap
-                    int temp = image[i][left];
-                    image[i][left] = image[i][right];
-                    image[i][right] = temp;
-                    //direct toggle
-                    image[i][left] ^= 1;
-                    image[i][right] ^= 1;
-                }
+        // int n = image[0].size();
+        // for(int i=0;i<image.size();i++){
+        //     int left =0, right = n-1;
+        //     while(left <= right){
+        //         if(left == right){
+        //             image[i][left] ^= 1;
+        //         }
+        //         else{
+        //             //swap
+        //             int temp = image[i][left];
+        //             image[i][left] = image[i][right];
+        //             image[i][right] = temp;
+        //             //direct toggle
+        //             image[i][left] ^= 1;
+        //             image[i][right] ^= 1;
+        //         }
                 
-                left++,right--;
-            }       
+        //         left++,right--;
+        //     }       
+        // }
+
+        //with inbuild concise code
+        for(auto& row: image){
+            reverse(row.begin(),row.end());
+            for(auto& col: row){
+                col ^= 1;
+            }
         }
         return image;
     }
