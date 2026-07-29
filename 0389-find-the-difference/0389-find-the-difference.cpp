5d4f59b1ac9dbd36669bb351d9f1b183e35e0c61
Simran Parween <simranparween2754@gmail.com>
2026-07-29 15:01:42 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for(char c : t) sum +=c;
+        for(char c : s) sum -=c;
+        return (char) sum;
+    }
+};
