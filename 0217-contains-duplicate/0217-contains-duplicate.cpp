fee5145642834afed9fbe85a7f9d4ccd9d1e8c21
Simran Parween <simranparween2754@gmail.com>
2026-08-01 16:08:22 +0530

    Time: 77 ms (23.9%), Space: 111.5 MB (16.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
+        //O(nlog n)
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //hash set
+        unordered_set<int> seen;
+        for(int num : nums){
+            if(seen.count(num)>0){
                 return true;
             }
+            seen.insert(num);
         }
         return false;
     }
